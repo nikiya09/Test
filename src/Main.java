a67543e17f2ecccd857a9c6a40688d46fa614756
nikiya <nikagovsha468@gmail.com>
2025-09-07 22:28:10 +0300

    Add loop

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] argv){
         System.out.println("hello world");
+        for (int i=0; i<10; i++){
+            System.out.println(i*i);
+        }
     }
 }
